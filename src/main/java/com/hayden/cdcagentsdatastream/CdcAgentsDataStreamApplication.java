089c33c32157b4817601975745a2fc56f872448c
hayden.rear <hayden.rear@gmail.com>
2025-05-27 13:12:12 -0400

    updated

---
+++ src/main/java/com/hayden/cdcagentsdatastream/CdcAgentsDataStreamApplication.java
@@ -0,0 +1,13 @@
+package com.hayden.cdcagentsdatastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcAgentsDataStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdcAgentsDataStreamApplication.class, args);
+    }
+
+}
